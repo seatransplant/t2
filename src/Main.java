376331ac084eb214e0614aa84eca0245fc8c1a71
seatransplant <stephen.costanzo@snhu.edu>
2017-03-07 17:21:18 -0800

    Removed Groovy Test Case
    Signed-off-by: seatransplant <stephen.costanzo@snhu.edu>

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+ /*   public void ShowNumber(int value)
+    {
+        System.out.println(value);
+    }
+   */
+
+    public int CalculateNumber(int firstValue, int secondValue) {
+        return firstValue + secondValue;
+    }
 }
+
